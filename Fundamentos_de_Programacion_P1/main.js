2eb8352f56e5ce4cba9d11700e1a9a6c38f3a8ca
juan camilo jimenez <juancamilojimenezj@gmail.com>
2020-11-17 18:48:49 -0500

    Se agregaron los ejercicios del GeekBook de javascript fundamentos de programacion parte uno

---
+++ Fundamentos_de_Programacion_P1/main.js
@@ -0,0 +1,65 @@
+'use-strict';
+
+//document.querySelector('h1').innerHTML= '!Hola mundo¡';
+
+// Ejercicio uno 
+//document.querySelector('h1').innerHTML='Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente.';
+
+
+/*let earnings = 12020;
+let expenses = 5342;
+let benefits = earnings - expenses;
+
+document.querySelector('h1').innerHTML=benefits;*/
+
+//Ejercicio dos
+/*
+const direccionMia='Calle 51';
+const direccionMia='Calle 52';
+
+const direccionMia='calle 51';
+const direccionCompañera='calle 52';
+*/
+
+//Ejercicio tres
+/*
+let parrafoHola= document.querySelector('.parrafo');
+
+parrafoHola.innerHTML= parrafoHola.innerHTML + ' Mundo¡';
+*/
+
+//Ejercicio cuatro
+/*
+let nombreLista = document.querySelector('h1');
+
+nombreLista.innerHTML= nombreLista.innerHTML +' '+ document.querySelector('.l2').innerHTML;
+*/
+
+//Ejercicio cinco
+/*
+let contraseña = document.querySelector('.contraseña')
+
+contraseña.innerHTML='********';
+*/
+
+//Ejercicio seis
+/*
+const agregoDatos= document.querySelector('.lorem')
+const datosAgregar= '<h1>Texto agregado desde Js</h1><img src="http://via.placeholder.com/350x150"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+
+agregoDatos.innerHTML=datosAgregar;
+*/
+
+//Ejercicio 7
+/*
+const agregarLi= document.querySelector('.list');
+const listAgregar= '<li>1</li><li>2</li><li>3</li>';
+agregarLi.innerHTML= listAgregar;
+*/
+
+//Ejercicio ocho
+/*
+const btnDos= document.querySelector('.btn2');
+btnDos.classList.add('btnopacidad');
+btnDos.classList.remove('btn2');
+*/
